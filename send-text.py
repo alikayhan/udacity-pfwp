701e2d0f8a1a6b6a49a0f8dab0df4283a5349d30
Ali Kayhan <ali.kayhan13@gmail.com>
2015-08-16 21:54:19 +0300

    Add send_text script

---
+++ send-text.py
@@ -0,0 +1,16 @@
+__author__ = 'alikayhan'
+
+from twilio.rest import TwilioRestClient
+
+account_sid = "your_account_sid"
+auth_token = "your_auth_token"
+client = TwilioRestClient(account_sid, auth_token)
+
+message = client.sms.messages.create(
+    body="Hello, dude!",
+    to="+16282226242",
+    from_="+16282226242"
+)
+
+print message.sid
+
